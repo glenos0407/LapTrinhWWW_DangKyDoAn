2038c8bd294491fde774bb136f4e35d7ee7249c9
Thu <luongthithu1227@gmail.com>
2020-06-28 17:23:53 +0700

    add dalSinhVien

---
+++ DAL/SinhVienDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DAL
+{
+    public class SinhVienDAL
+    {
+        DangKyDoAn_DBContext db;
+        public SinhVienDAL()
+        {
+            db = new DangKyDoAn_DBContext();
+        }
+        public SinhVien GetSinhVien(int id)
+        {
+            return db.SinhViens.FirstOrDefault(sv => sv.idSinhVien == id);
+        }
+        public string GetHoTenSinhVien(int id)
+        {
+            return db.SinhViens.FirstOrDefault(sv => sv.idSinhVien == id).hoTenSinhVien;
+        }
+        public bool SuaTTSinhVien(SinhVien sv)
+        {
+            try
+            {
+                SinhVien sinhvien = db.SinhViens.FirstOrDefault(x => x.idSinhVien == sv.idSinhVien);
+                sinhvien.diaChi = sv.diaChi;
+                sinhvien.soDienThoai = sv.soDienThoai;
+                sinhvien.email = sv.email;
+                db.SaveChanges();
+
+            }
+            catch 
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
